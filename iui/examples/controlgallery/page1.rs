0b77909b92e50b8a72a476e798a06cd0f724663f
Jakob K <jjk@outlook.at>
2023-01-25 19:28:04 +0100

    changed controlgallery to use the Form control

--- iui/examples/controlgallery/page1.rs
+++ iui/examples/controlgallery/page1.rs
@@ -21,144 +21,24 @@ pub fn make_basic_page(ui: UI) -> Control {
     vbox.append(&ui, lb, LayoutStrategy::Compact);
     vbox.append(&ui, hs, LayoutStrategy::Compact);
 
-    let mut grid = LayoutGrid::new(&ui);
-    grid.set_padded(&ui, true);
+    let mut form = Form::new(&ui);
+    form.set_padded(&ui, true);
 
-    // Column 0 - Label
-    {
-        let lb_entry = Label::new(&ui, "Entry");
-        let lb_entry_pwd = Label::new(&ui, "Password Entry");
-        let lb_entry_search = Label::new(&ui, "Search Entry");
-        let lb_entry_multi = Label::new(&ui, "Multiline Entry");
-        let lb_entry_nowrap = Label::new(&ui, "Non-wrapping Entry");
+    let entry = Entry::new(&ui);
+    let entry_pwd = PasswordEntry::new(&ui);
+    let entry_search = SearchEntry::new(&ui);
+    let entry_multi = MultilineEntry::new(&ui);
+    let entry_nowrap = MultilineEntry::new(&ui);
 
-        grid.append(
-            &ui,
-            lb_entry.clone(),
-            0,
-            0,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            lb_entry_pwd.clone(),
-            0,
-            1,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            lb_entry_search.clone(),
-            0,
-            2,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            lb_entry_multi.clone(),
-            0,
-            3,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            lb_entry_nowrap.clone(),
-            0,
-            4,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-    };
-
-    // Column 1 - Inputs
-    let (entry, entry_pwd, entry_search, entry_multi, entry_nowrap) = {
-        let entry = Entry::new(&ui);
-        let entry_pwd = PasswordEntry::new(&ui);
-        let entry_search = SearchEntry::new(&ui);
-        let entry_multi = MultilineEntry::new(&ui);
-        let entry_nowrap = MultilineEntry::new(&ui);
-
-        grid.append(
-            &ui,
-            entry.clone(),
-            1,
-            0,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            entry_pwd.clone(),
-            1,
-            1,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            entry_search.clone(),
-            1,
-            2,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            entry_multi.clone(),
-            1,
-            3,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-        grid.append(
-            &ui,
-            entry_nowrap.clone(),
-            1,
-            4,
-            1,
-            1,
-            GridExpand::Neither,
-            GridAlignment::Fill,
-            GridAlignment::Fill,
-        );
-
-        (entry, entry_pwd, entry_search, entry_multi, entry_nowrap)
-    };
-
+    form.append(&ui, "Entry", entry, LayoutStrategy::Compact);
+    form.append(&ui, "Password Entry", entry_pwd, LayoutStrategy::Compact);
+    form.append(&ui, "Search Entry", entry_search, LayoutStrategy::Compact);
+    form.append(&ui, "Multiline Entry", entry_multi, LayoutStrategy::Compact);
+    form.append(&ui, "Non-wrapping Entry", entry_nowrap, LayoutStrategy::Compact);
+        
     let mut group = Group::new(&ui, "Entries");
     group.set_margined(&ui, true);
-    group.set_child(&ui, grid);
+    group.set_child(&ui, form);
 
     vbox.append(&ui, group, LayoutStrategy::Stretchy);
 
